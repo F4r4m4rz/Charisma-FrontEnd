f4b06a535474fe1e8a2044100162a58ff7df5760
Faramarz Bodaghi <faramarz.bodaghi@outlook.com>
2019-12-27 21:29:11 +0100

    Trying to pass model into the view

---
+++ Charisma.UI.MVC/Controllers/MenuController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Charisma.Data.Data;
+using Charisma.Data.InMemory;
+using Charisma.UI.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Charisma.UI.MVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly ICharismaData db;
+
+        public MenuController()
+        {
+            this.db = new MenuItemData();
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var model = db.GetAll();
+            return View(model);
+        }
+    }
+}
